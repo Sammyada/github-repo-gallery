1cb39dc9e68cadde6f140d14af11c2d7f35447bf
sammyada <g.sammyada@gmail.com>
2021-12-07 22:15:34 -0500

    Attached users info onto website.

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// where my profile information will appear
+const overview = document.querySelector(".overview");
+const username = "sammyada";
+
+// fetch api json data
+const gitUserInfo = async function () {
+  const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    userDisplayInfo(data);
+};
+
+gitUserInfo();
+
+// fetch and display my information
+const userDisplayInfo = function (data) {
+  const div = document.createElement("div");
+  div.classList.add("user-info");
+  div.innerHTML = `
+  <figure>
+     <img alt="user avatar" src=${data.avatar_url} />
+   </figure>
+   <div>
+     <p><strong>Name:</strong> ${data.name}</p>
+     <p><strong>Bio:</strong> ${data.bio}</p>
+     <p><strong>Location:</strong> ${data.location}</p>
+     <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+   </div>
+  `;
+
+  overview.append(div);
+};
